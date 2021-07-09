90de5073330a47c1159e829acbc78b06d07b033e
Akwasi Appiah <akwasi.atta.appiah@gmail.com>
2021-07-09 15:38:45 +0100

    Added Season and episode to dropdown

--- src/components/DropdownBox.tsx
+++ src/components/DropdownBox.tsx
@@ -5,7 +5,10 @@ export const DropdownBox = ({ eds }: dropdownProps): JSX.Element => {
     <>
       <select id="episodes">
         {eds.map((episode) => (
-          <option>{episode.name}</option>
+          <option>
+            {episode.name}, {episode.season < 10 ? `S0${episode.season}` : `S${episode.season}`}
+              {episode.number < 10 ? `E0${episode.number}` : `E${episode.number}`}
+          </option>
         ))}
       </select>
     </>
